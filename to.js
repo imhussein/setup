2ef032d98a0c27ac0b551005af57b6e087ba5fbb
Mohamed Hussien <yh10445@gmail.com>
2021-01-18 19:21:08 +0400

    Update to.js

--- to.js
+++ to.js
@@ -4,8 +4,13 @@
 // Bash - Unix - Github
 // ML - AI
 // Review
-// React And Express
-// React Framework
-// NodeJS Framework
+// React And Express Explore
 // The Book - Functional Programming
 // Networking
+
+// March
+// 1 - Golang App
+// 2 - More GoLang
+// 3 - React Explore
+// 4 - Github + Bash
+// 5 - Python Review
